d76674dc141c1a37a7283e1cb34495bbbb6381ed
Mike Watts <awarebayes@gmail.com>
2019-08-24 14:49:30 +0300

    indexes and dist tests added
    Former-commit-id: 2fc63d52fa4946b96f11194fa97bdb7f061b3192

---
+++ utils/models.py
@@ -0,0 +1,146 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+class Actor(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_size, init_w=2e-1):
+        super(Actor, self).__init__()
+        
+        self.drop_layer = nn.Dropout(p=0.5)
+        
+        self.linear1 = nn.Linear(num_inputs, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(hidden_size, num_actions)
+        
+        self.linear3.weight.data.uniform_(-init_w, init_w)
+        self.linear3.bias.data.uniform_(-init_w, init_w)
+        
+    def forward(self, state):
+        # state = self.state_rep(state)
+        x = F.relu(self.linear1(state))
+        x = self.drop_layer(x)
+        x = F.relu(self.linear2(x))
+        x = self.drop_layer(x)
+        # x = torch.tanh(self.linear3(x)) # in case embeds are -1 1 normalized
+        x = self.linear3(x) # in case embeds are standard scaled / wiped using PCA whitening
+        # return state, x
+        return x
+    
+class AnomalyDetector(nn.Module):
+    def __init__(self):
+        super(AnomalyDetector, self).__init__()
+        self.ae = nn.Sequential(
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.BatchNorm1d(64),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.BatchNorm1d(32),
+            nn.Linear(32, 64),
+            nn.ReLU(),
+            nn.BatchNorm1d(64),
+            nn.Linear(64, 128),
+            nn.ReLU(),
+        )
+        
+    def forward(self, x):
+        return self.ae(x)
+    
+    def rec_error(self, x):
+        error = torch.sum((x - self.ae(x)) ** 2, 1)
+        if x.size(1) != 1:
+            return error.detach()
+        return error.item()
+    
+class Critic(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_size, init_w=3e-5):
+        super(Critic, self).__init__()
+        
+        self.drop_layer = nn.Dropout(p=0.5)
+        
+        self.linear1 = nn.Linear(num_inputs + num_actions, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(hidden_size, 1)
+        
+        self.linear3.weight.data.uniform_(-init_w, init_w)
+        self.linear3.bias.data.uniform_(-init_w, init_w)
+        
+    def forward(self, state, action):
+        x = torch.cat([state, action], 1)
+        x = F.relu(self.linear1(x))
+        x = self.drop_layer(x)
+        x = F.relu(self.linear2(x))
+        x = self.drop_layer(x)
+        x = self.linear3(x)
+        return x
+    
+class BCQ_Actor(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_size, init_w=3e-1):
+        super(BCQ_Actor, self).__init__()
+        
+        self.drop_layer = nn.Dropout(p=0.5)
+        
+        self.linear1 = nn.Linear(num_inputs + num_actions, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(hidden_size, num_actions)
+        
+        self.linear3.weight.data.uniform_(-init_w, init_w)
+        self.linear3.bias.data.uniform_(-init_w, init_w)
+        
+    def forward(self, state, action):
+        a = torch.cat([state, action], 1)
+        a = F.relu(self.linear1(a))
+        a = self.drop_layer(a)
+        a = F.relu(self.linear2(a))
+        a = self.drop_layer(a)
+        a = self.linear3(a) 
+        return a + action
+    
+class BCQ_VAE(nn.Module):
+    def __init__(self, state_dim, action_dim, latent_dim):
+        super(BCQ_VAE, self).__init__()
+        #encoder
+        self.e1 = nn.Linear(state_dim + action_dim, 750)
+        self.e2 = nn.Linear(750, 750)
+
+        self.mean = nn.Linear(750, latent_dim)
+        self.log_std = nn.Linear(750, latent_dim)
+        
+        #decoder
+        self.d1 = nn.Linear(state_dim + latent_dim, 750)
+        self.d2 = nn.Linear(750, 750)
+        self.d3 = nn.Linear(750, action_dim)
+        
+        self.latent_dim = latent_dim
+        self.normal = torch.distributions.Normal(0, 1)
+
+
+    def forward(self, state, action):
+        # z is encoded state + action
+        z = F.relu(self.e1(torch.cat([state, action], 1)))
+        z = F.relu(self.e2(z))
+
+        mean = self.mean(z)
+        # Clamped for numerical stability 
+        log_std = self.log_std(z).clamp(-4, 15)
+        std = torch.exp(log_std)
+        z = mean + std * self.normal.sample(std.size()).to(next(self.parameters()).device)
+
+        # u is decoded action
+        u = self.decode(state, z)
+
+        return u, mean, std
+    
+    def decode(self, state, z=None):
+        # When sampling from the VAE, the latent vector is clipped to [-0.5, 0.5]
+        if z is None:
+            z = self.normal.sample([state.size(0), self.latent_dim])
+            z = z.clamp(-0.5, 0.5).to(next(self.parameters()).device)
+
+
+        a = F.relu(self.d1(torch.cat([state, z], 1)))
+        a = F.relu(self.d2(a))
+        return self.d3(a)
